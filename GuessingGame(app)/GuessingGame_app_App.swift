35b26f8e0372d8e792fa8e82be0c882aab3cb979
Ayo <aogunkinle@lcs.on.ca>
2024-02-02 09:33:14 -0500

    Initial Commit

---
+++ GuessingGame(app)/GuessingGame_app_App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGame_app_App.swift
+//  GuessingGame(app)
+//
+//  Created by Ayodeji Ogunkinle on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGame_app_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
